a16da2cd7b5bd5b8110e6313c41b76349317242b
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2025-04-04 18:25:36 +0330

    coding NavBar(HTML,CSS,JS)

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
-document.querySelectorAll('.navMenu li').forEach(item => {
-  item.addEventListener('click' , function(event){
-    event.stopPropagation();
-  }) 
-  let dropdown=this.querySelector('.dropdown');
-  if(drowpdown.style.display === "block"){
-    drowpdown.style.display="none"
-  }
-  
-})
+//use reload for first showing site;
+const loaderElem=document.querySelector('.loaderHidden');
+window.addEventListener('load', function(){
+  this.setTimeout(() => {
+this.document.querySelector(".loaderHidden").style.display="none";},3000);
+  });
+
+//reload and Refresh the site.
+document.querySelector('.linkitem.active').addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = 'index.html'; // هدایت به صفحه اصلی
+});
